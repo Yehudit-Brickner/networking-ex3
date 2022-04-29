7e370a84d1ac1ff5e32117c892c84a063c60805d
Yehudit-Brickner <nech.diti@yahoo.com>
2022-04-29 16:53:39 +0300

    start

--- assignment3.py
+++ assignment3.py
@@ -5,51 +5,55 @@ import sys # In order to terminate the program
 serverSocket = socket(AF_INET, SOCK_STREAM)
 #Prepare a sever socket
 #Fill in start
-serverName = 'OurServer'
-serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_STREAM)
+SERVER_ADDRESS = ('0.0.0.0', 13000)
+serverSocket.bind(SERVER_ADDRESS)
+serverSocket.listen(1)
 #Fill in end
 
 while True:
     #Establish the connection
     print('Ready to serve...')
-    connectionSocket, addr =   clientSocket.connect((serverName,serverPort))
-    serverSocket.bind((‘0.0.0.0’,serverPort))
-    serverSocket.listen(1)
+    #Fill in start
+    connectionSocket, addr = serverSocket.accept()
+    print("made a connection")
+    #Fill in end
 
-    #Fill in start              #Fill in end          
+
     try:
-        message =   #Fill in start          #Fill in end               
-        filename = message.split()[1]                 
-        f = open(filename[1:])                        
-        outputdata = #Fill in start       #Fill in end                   
-        #Send one HTTP header line into socket
-        #Fill in start
-        #Fill in end                
-        #Send the content of the requested file to the client
-        for i in range(0, len(outputdata)):           
-            connectionSocket.send(outputdata[i].encode())
-        connectionSocket.send("\r\n".encode())
-        
-        connectionSocket.close()
+        print("im in the try")
+    #     #Fill in start
+    #     message = input('please enter a file name')
+    #     #Fill in end
+    #     filename = message.split()[1]
+    #     f = open(filename[1:])
+    #     #Fill in start
+    #     outputdata =
+    #     #Fill in end
+    #
+    #     #Send one HTTP header line into socket
+    #     #Fill in start
+    #     #Fill in end
+    #
+    #     #Send the content of the requested file to the client
+    #     for i in range(0, len(outputdata)):
+    #         connectionSocket.send(outputdata[i].encode())
+    #     connectionSocket.send("\r\n".encode())
+    #
+    #     connectionSocket.close()
     except IOError:
+        print("exception caught")
         #Send response message for file not found
-        #Fill in start        
+        #Fill in start
+        # connectionSocket.send(bytes("404 Not Found"))
         #Fill in end
+
         #Close client socket
         #Fill in start
-        #Fill in end            
+        # clientSocket.close()
+        # print("closed client socket")
+        #Fill in end
+
 serverSocket.close()
+print("closed server socket")
 sys.exit()#Terminate the program after sending the corresponding data                                    
 
-
-# from socket import *
-# serverName = 'servername'
-# serverPort = 12000
-# clientSocket = socket(AF_INET, SOCK_STREAM)
-# clientSocket.connect((serverName,serverPort))
-# sentence = input('Input lowercase sentence:')
-# clientSocket.send(sentence.encode())
-# modifiedSentence = clientSocket.recv(1024)
-# print ('From Server:', modifiedSentence.decode())
-# clientSocket.close()
