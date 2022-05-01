63e4214d906e1549d9aaffa82795070ef9ac7ac8
Yehudit-Brickner <nech.diti@yahoo.com>
2022-05-01 16:43:23 +0300

    filled in more

--- assignment3.py
+++ assignment3.py
@@ -5,7 +5,7 @@ import sys # In order to terminate the program
 serverSocket = socket(AF_INET, SOCK_STREAM)
 #Prepare a sever socket
 #Fill in start
-SERVER_ADDRESS = ('0.0.0.0', 13000)
+SERVER_ADDRESS = ('0.0.0.0', 12000)
 serverSocket.bind(SERVER_ADDRESS)
 serverSocket.listen(1)
 #Fill in end
@@ -15,41 +15,46 @@ while True:
     print('Ready to serve...')
     #Fill in start
     connectionSocket, addr = serverSocket.accept()
-    print("made a connection")
+    print("made a connection with ", connectionSocket, addr)
     #Fill in end
 
 
     try:
-        print("im in the try")
-    #     #Fill in start
-    #     message = input('please enter a file name')
-    #     #Fill in end
-    #     filename = message.split()[1]
-    #     f = open(filename[1:])
-    #     #Fill in start
-    #     outputdata =
-    #     #Fill in end
-    #
-    #     #Send one HTTP header line into socket
-    #     #Fill in start
-    #     #Fill in end
-    #
-    #     #Send the content of the requested file to the client
-    #     for i in range(0, len(outputdata)):
-    #         connectionSocket.send(outputdata[i].encode())
-    #     connectionSocket.send("\r\n".encode())
-    #
-    #     connectionSocket.close()
+        print("/nim in the try/n ")
+        #Fill in start
+        message = connectionSocket.recv(4096).decode()
+        # print(message)
+        #Fill in end
+        filename = message.split()[1]
+        if (filename[1:] == 'favicon.ico'):
+            continue
+        print(filename)
+        f = open(filename[1:])
+        #Fill in start
+        outputdata = f.read()
+        #Fill in end
+
+        # Send one HTTP header line into socket
+        # Fill in start
+        connectionSocket.send("HTTP/1.1 200 OK\n".encode())
+        # Fill in end
+
+        #Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        connectionSocket.send("\r\n".encode())
+
+        connectionSocket.close()
     except IOError:
         print("exception caught")
         #Send response message for file not found
         #Fill in start
-        # connectionSocket.send(bytes("404 Not Found"))
+        connectionSocket.send(bytes("404 Not Found\n"))
         #Fill in end
 
         #Close client socket
         #Fill in start
-        # clientSocket.close()
+        connectionSocket.close()
         # print("closed client socket")
         #Fill in end
 
